48c3e46cfbbf5c35d6b62e1f30f06bd6702cb048
Gerasimova Olga <olga.g3993@gmail.com>
2021-11-20 14:30:12 +0300

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,7 @@
+from .cora import Cora
+from .citeseer import CiteseerM10
+from .dblp import Dblp
+from .scopus_econ import ScopusEcon
+from .ann import Ann
+
+__all__ = ['Cora', 'CiteseerM10', 'Dblp', 'ScopusEcon', 'Ann']
